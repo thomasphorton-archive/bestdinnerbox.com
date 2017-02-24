e0bb04c1dabfbae14b58bbce09bab90c28932d5e
Thomas P. Horton <thomasphorton@gmail.com>
2017-02-23 20:16:25 -0800

    Getting started

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug2');
 const sass = require('gulp-sass');
+const awsConfig = require('./config/aws');
+const s3 = require('gulp-s3-upload')(awsConfig);
 
 // SASS tasks
 gulp.task('sass', function buildStyles() {
@@ -19,14 +21,24 @@ gulp.task('sass:watch', function() {
 
 // Pug tasks
 gulp.task('views', function buildHTML() {
-  return gulp.src('./views/*.pug')
-    .pipe(pug())
+  return gulp.src('./views/**/!(_)*.pug')
+    .pipe(pug({
+      basedir: './views/'
+    }))
     .pipe(gulp.dest('build'))
 });
 
 gulp.task('views:watch', function() {
   gulp.watch('./views/**/*.pug', ['views']);
 })
+
+gulp.task('deploy', function() {
+  gulp.src('./build/**')
+    .pipe(s3({
+      Bucket: 'bestdinnerbox.com',
+      ACL: 'public-read'
+    }));
+});
 
 gulp.task('watch', ['sass:watch', 'views:watch']);
 
